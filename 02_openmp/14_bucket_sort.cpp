6d3e463e4b95d945e3186a0cc16d7bb473d6d0ae
wakana-ymd <yamadaw.r921@gmail.com>
2026-04-20 13:27:04 +0900

    homework for lecture2

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
   int n = 50;
@@ -18,10 +19,13 @@ int main() {
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
+
+  #pragma omp parallel for shared(bucket,offset,key)
   for (int i=0; i<range; i++) {
     int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
+    int cnt = bucket[i];
+    for (int k=0; k<cnt; k++) {
+      key[j+k] = i;
     }
   }
 
